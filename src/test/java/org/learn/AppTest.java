6890b0f39f3ce9e2817b200228ccf9b4981c3821
AdarshGowdaS <adarsh.s1@bigbasket.com>
2023-12-18 20:30:15 +0530

    Added launch browser test method

---
+++ src/test/java/org/learn/AppTest.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.setProperty("webdriver.chrome.driver", "/home/adarshs/selenium_jars_drivers/drivers/cromedriver/chromedriver_linux64/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+
+
+        // Maximize the browser window
+        driver.manage().window().maximize();
+
+
+        // Navigate to the Investing.com login page
+        // Navigate to investing.com
+        driver.get("https://demoqa.com/");
+        System.out.println("in");
+        // Find and click on the 'Login' link (or button) to open the login form
+        WebElement loginButton = driver.findElement(By.linkText("Login"));
+        loginButton.click();
+
+        // Find the username and password fields and input credentials
+        WebElement usernameField = driver.findElement(By.id("loginFormUser_email"));
+        usernameField.sendKeys("your_username");
+
+        WebElement passwordField = driver.findElement(By.id("loginForm_password"));
+        passwordField.sendKeys("your_password");
+
+        // Find and click the 'Login' button to submit the credentials
+        WebElement submitButton = driver.findElement(By.id("loginForm_btn"));
+        submitButton.click();
+
+
+        driver.quit();
+    }
+    }
+
---
+++ src/test/java/org/learn/selenium/tests/BrowserLaunchTest.java
@@ -0,0 +1,45 @@
+package org.learn.selenium.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class BrowserLaunchTest {
+
+    private WebDriver driver;
+
+
+    @BeforeClass
+    public void setUpClass() {
+        // Setup WebDriverManager for ChromeDriver
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeTest
+    public void setUpTest() {
+        // Initialize ChromeDriver
+        driver = new ChromeDriver();
+    }
+
+    @BeforeMethod
+    public void setUpMethod() {
+        // Optional: Maximize the browser window
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void launchBrowser(){
+        // Navigate to a webpage
+        driver.get("https://www.google.com");
+
+        // Close the browser
+        driver.quit();
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
+        // Close the browser after each test method
+        driver.quit();
+    }
+}
