e4a7f1afa7ad99cada67db5907f86ab48184c814
AdarshGowdaS <adarsh.s1@bigbasket.com>
2023-12-19 09:59:54 +0530

    Added simple tests

--- src/test/java/org/learn/AppTest.java
+++ src/test/java/org/learn/AppTest.java
@@ -1,12 +1,10 @@
-package org.example;
+package org.learn;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
